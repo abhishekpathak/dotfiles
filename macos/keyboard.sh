4aba0cad93dd3f2208c6fc4ad41ae0648ba0b66c
Abhishek Pathak <me@abhishekpathak.net>
2017-10-26 18:56:15 +0530

    fix : set key repeat to not be so blazingly fast

--- macos/keyboard.sh
+++ macos/keyboard.sh
@@ -5,4 +5,4 @@ echo "Disable press-and-hold for keys in favor of key repeat."
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 echo "Set a really fast key repeat."
-defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 5
