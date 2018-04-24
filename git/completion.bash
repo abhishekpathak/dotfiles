5017fceeb4178e88fd93c61f88a6df7cde9cf13a
Abhishek Pathak <me@abhishekpathak.net>
2018-04-25 00:14:27 +0530

    bugfix

--- git/completion.bash
+++ git/completion.bash
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Add tab completion for bash completion 2
-if which brew > /dev/null && [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ]; then
-  source "$(brew --prefix)/share/bash-completion/bash_completion";
+if which brew > /dev/null && [ -f "$(brew --prefix)/etc/bash_completion" ]; then
+  source "$(brew --prefix)/etc/bash_completion";
 elif [ -f /etc/bash_completion ]; then
   source /etc/bash_completion;
 fi;
