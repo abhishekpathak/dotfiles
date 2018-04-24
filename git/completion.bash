df204519d2640cb4e1448606b3ca4d5fa44850a8
Abhishek Pathak <me@abhishekpathak.net>
2018-04-24 23:52:35 +0530

    incremental updates

--- git/completion.bash
+++ git/completion.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# load git completion
-if [ -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]; then
-  . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
-fi
+# Add tab completion for bash completion 2
+if which brew > /dev/null && [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ]; then
+  source "$(brew --prefix)/share/bash-completion/bash_completion";
+elif [ -f /etc/bash_completion ]; then
+  source /etc/bash_completion;
+fi;
