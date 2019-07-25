b8cc560bd17fdc924d8e5e43ccfe02e572f975d3
Abhishek Pathak <4abhishekpathak@gmail.com>
2019-07-25 20:55:35 +0530

    switch to powerlevel9k prompt

--- apps/skymind/env.sh
+++ apps/skymind/env.sh
@@ -9,14 +9,14 @@ export SKIL_GCP_CRED_FILE=$CLOUD_CREDS_DIR/skil_gcp_cred.json
 #export SKIL_PUBLIC_KEY_PATH=/Users/abhishek/code/skymind/skil-server/skil-test-resources/src/main/resources/skil-trial-public.txt
 #export SKIL_CLASS_PATH=$SKIL_HOME/lib/*:$SKIL_HOME/native/*:$SKIL_HOME/lib/ext/*:$SKIL_HOME/jackson-2.5.1/*
 #   
-export SKIL_DEBUG_MODE=false
-export SKIL_DEBUG_PORT=8500
-export SKIL_DEBUG_FREEZE=n
+#export SKIL_DEBUG_MODE=false
+#export SKIL_DEBUG_PORT=8500
+#export SKIL_DEBUG_FREEZE=n
 # alias to  update a skil subpackage in SKIL_HOME
 alias skilup="mvn clean package -DskipTests && cp $PWD/target/*.jar $SKIL_HOME/lib && $SKIL_HOME/sbin/stop-skil-daemon && sleep 10 && $SKIL_HOME/sbin/start-skil-daemon"
-export SPARK_HOME="/opt/spark"
-export HADOOP_HOME="/opt/hadoop"
-export SPARK_MASTER_WEBUI_PORT=9095
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+#export SPARK_HOME="/opt/spark"
+#export HADOOP_HOME="/opt/hadoop"
+#export SPARK_MASTER_WEBUI_PORT=9095
+#export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 #export HADOOP_NAMENODE_HOST="127.0.0.1"
 #export HADOOP_NAMENODE_PORT="8020"
