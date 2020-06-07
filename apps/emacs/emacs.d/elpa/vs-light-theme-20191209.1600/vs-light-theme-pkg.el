864d9eb5934c0ba1bebbe95267576f6e7cd583e0
Abhishek Pathak <4abhishekpathak@gmail.com>
2020-06-07 21:00:22 +0530

    add emacs config

---
+++ apps/emacs/emacs.d/elpa/vs-light-theme-20191209.1600/vs-light-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vs-light-theme" "20191209.1600" "Visual Studio IDE light theme" '((emacs "24.1")) :commit "764f478f5866140b121b1e43857487b7a66afa72" :authors '(("Jen-Chieh Shen")) :maintainer '("Jen-Chieh Shen") :url "https://github.com/jcs090218/vs-light-theme")
