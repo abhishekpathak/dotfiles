6f91d7c462c6eae3f3294acc6c6c7438f42e3926
Abhishek Pathak <4abhishekpathak@gmail.com>
2020-06-18 13:27:15 +0530

    update emacs config

---
+++ apps/emacs/emacs.d/elpa/auto-save-buffers-enhanced-20161109.710/auto-save-buffers-enhanced-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-save-buffers-enhanced" "20161109.710" "Automatically save buffers in a decent way" 'nil :commit "461e8c816c1b7c650be5f209078b381fe55da8c6" :authors '(("Kentaro Kuribayashi" . "kentarok@gmail.com")) :maintainer '("Kentaro Kuribayashi" . "kentarok@gmail.com"))
