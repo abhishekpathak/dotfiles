489d89c3931f17401e9d5120aba121ca15de6dff
Abhishek Pathak <4abhishekpathak@gmail.com>
2021-06-26 15:22:49 +0530

    git: add function to prune gone branches

--- apps/git/env.sh
+++ apps/git/env.sh
@@ -9,3 +9,12 @@ alias gco='git checkout'
 alias gd='git diff'
 alias gshow='git show'
 alias gshowf='git show --name-status'
+
+
+git_prune_gone_branches() {
+    actually_delete="$1"
+    git fetch -p
+    branches=$(git branch -av | grep gone | awk '{ print $1 }')
+    echo -e "Gone branches:\n\n$branches"
+    if [[ "$actually_delete" == "--delete" ]] ; then echo "Deleting..." ; echo $branches | xargs -L 1 git branch -D ; else echo "\n\nThis was a dry run. Use --delete to remove these branches." ; fi
+}
