49cb06d5d556a0c4f583f16cc8a9ceb19c9c63a3
Abhishek Pathak <4abhishekpathak@gmail.com>
2022-12-29 11:11:41 +0530

    commit checkpoint before trimming

--- apps/airbase/env.sh
+++ apps/airbase/env.sh
@@ -1,2 +1,6 @@
 # Airbase-specific
 export XERO_RSA_KEY_PATH=$CODE/airbase/airbase-backend/.xero.pem
+#alias stagedbrestore='pg_restore --verbose --clean --no-acl --no-owner -h localhost -U airbase -d airbase staging.dump'
+#alias stagedbdownload='task sync_db --dbname=staging --no-sync-local-db --no-update-env'
+export DJANGO_SETTINGS_MODULE=airbase_backend.settings.local_settings
+export DD_TRACE_ENABLED=False
