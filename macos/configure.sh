d68b82a719796d8ed2a43f75ba5b91dfc53e2df3
Abhishek Pathak <4abhishekpathak@gmail.com>
2024-02-14 13:57:04 +0530

    update homebrew install command

--- macos/configure.sh
+++ macos/configure.sh
@@ -5,7 +5,7 @@ set -e
 if test ! $(which brew)
 then
   echo "Installing Homebrew for you..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew seems to be already installed."
 fi
