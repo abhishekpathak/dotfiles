d9095857a61e5925e887e767129d71cf8b99e389
Abhishek Pathak <4abhishekpathak@gmail.com>
2024-11-18 13:16:20 +0530

    fix

--- apps/zsh/configure.sh
+++ apps/zsh/configure.sh
@@ -5,7 +5,7 @@ echo "configuring zsh ..."
 ln -sf $DOTFILES/apps/zsh/zshrc $HOME/.zshrc
 
 echo "syncing zsh history.."
-ln -sf ~$WORKSPACE/zsh_history ~/.zsh_history
+ln -sf $WORKSPACE/zsh_history ~/zsh_history
 
 echo "execute this manually: chsh -s $(which zsh) "
 echo "and then logout and log back in to set zsh as the default shell."
